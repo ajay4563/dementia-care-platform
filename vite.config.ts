5bd4607adff28b0d28ef8536d235b611b4f3c997
ajay21 <ajayrajesh4563@gmail.com>
2026-09-05 18:11:56 +0530

    fix corrupted

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-ajayr25cse_db_user
-1Oj7TRtBIjxQyYo2
-mongodb+srv://<db_username>:1Oj7TRtBIjxQyYo2@cluster0.ropjctm.mongodb.net/?appName=Cluster0
-mongodb atlas password :mongodb+srv://ajayr25cse_db_user:1Oj7TRtBIjxQyYo2
-@cluster0.ropjctm.mongodb.net/?appName=Cluster0
-vercel recovery codes
-078e4cf9-81a93d42
-fe414278-42561b16
-0cf353e7-044541ba
-8761d323-ebd3cac5
-2ba99f65-b43600d8
-e9681c76-257018ce
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
